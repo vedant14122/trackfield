c2910ea5d1190bb762b82d49b7f4ba8130f02678
vedant14122 <vedant_kharul@myabcusd.org>
2025-06-30 16:34:45 -0700

    Update main.dart

--- athlete_form_ai/frontend/lib/main.dart
+++ athlete_form_ai/frontend/lib/main.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'pages/upload_page.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Track & Field AI',
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: UploadPage(),
+    );
+  }
+}
