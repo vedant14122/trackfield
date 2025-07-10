ab7b2a0b8bd8c7f4952e4b96929e20f774dfc41d
vedant14122 <vedant_kharul@myabcusd.org>
2025-07-09 21:36:44 -0700

    Update supabase_client.py

--- athlete_form_ai/backend/app/utils/supabase_client.py
+++ athlete_form_ai/backend/app/utils/supabase_client.py
@@ -1,7 +1,6 @@
-import os
 from supabase import create_client, Client
+import os
 
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
-
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+url = os.environ.get("SUPABASE_URL")
+key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
+supabase: Client = create_client(url, key)
